25eff3f3d640dab38117d1b19e7f8a2eb7ddc2ad
DiperCool <githubdiper@gmail.com>
2021-07-14 21:00:08 +0200

    add to basket in img

--- wwwroot/js/Android/Home.js
+++ wwwroot/js/Android/Home.js
@@ -15,13 +15,48 @@ let loadProducts=()=>{
     });
 }
 loadProducts();
-
+document.addEventListener("click",(e)=>{
+    let classList = e.target.classList;
+    if(classList.contains("img-product")) return;
+    clearActiveContainer();
+})
+var containers = document.querySelectorAll(".container-img-product");
+const  clearActiveContainer = ()=>{
+    containers.forEach(el=>{
+        let button = el.querySelector(".img-product-button");
+        let middle = el.querySelector(".middle");
+        let photo = el.querySelector(".img-product");
+        button.classList.remove("img-product-button-active");
+        middle.classList.remove("middle-active");
+        photo.classList.remove("img-product-active");
+        el.classList.remove("container-img-product-active");
+    })
+    console.log("clear")
+}
+containers.forEach(el=>{
+    let photo = el.querySelector(".img-product");
+    el.addEventListener("click",()=>{
+        if(photo.classList.contains("img-product-active")){
+            clearActiveContainer();
+            return;
+        }
+        clearActiveContainer();
+        let button = el.querySelector(".img-product-button");
+        let middle = el.querySelector(".middle");
+        
+        button.classList.add("img-product-button-active");
+        middle.classList.add("middle-active");
+        photo.classList.add("img-product-active");
+        el.classList.add("container-img-product-active");
+    });
+})
 
 let setOnClick= ()=>{
     let pluses = document.querySelectorAll(".button-product");
     pluses.forEach(el=>{
         el.addEventListener("click",async(e)=>{
             e.stopPropagation();
+            clearActiveContainer();
             let currentTarget = e.currentTarget;
             if(currentTarget.getAttribute("disabled")){
                 console.log("oleg oelg oegl");
@@ -50,3 +85,22 @@ let setOnClick= ()=>{
 }
 
 setOnClick();
+
+let buttonsImg=document.querySelectorAll(".img-product-button");
+buttonsImg.forEach((el)=>{
+    el.addEventListener("click",async (e)=>{
+        e.stopPropagation();
+        let currentTarget = e.currentTarget;
+        if(currentTarget.getAttribute("disabled")){
+            console.log("oleg oelg oegl");
+            return;
+        }
+        currentTarget.setAttribute("disabled","true");
+        let id = Number.parseInt(el.getAttribute("data-id"));
+        await addProduct(id);
+        loadProducts();
+        currentTarget.removeAttribute("disabled");
+        clearActiveContainer();
+
+    })
+})
